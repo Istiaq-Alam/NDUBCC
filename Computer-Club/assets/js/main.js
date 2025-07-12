4f358fb4c5e582cb252a9f4cf4e29e014414856d
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2025-07-12 12:03:24 +0600

    header fixed

--- Computer-Club/assets/js/main.js
+++ Computer-Club/assets/js/main.js
@@ -110,14 +110,14 @@
 // include.js
 
 // Load Header
-fetch('/ComputerClub/assets/header.html')
+fetch('assets/header.html')
   .then(res => res.text())
   .then(text => {
     document.getElementById('header').innerHTML = text;
   });
 
 // Load Footer
-fetch('/ComputerClub/assets/footer.html')
+fetch('assets/footer.html')
   .then(res => res.text())
   .then(text => {
     document.getElementById('footer').innerHTML = text;
