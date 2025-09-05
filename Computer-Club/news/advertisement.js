9230145c92c1cdab6c3081c6a6c8b7c70ef0afde
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2025-09-06 00:30:42 +0600

    Update advertisement.js

--- Computer-Club/news/advertisement.js
+++ Computer-Club/news/advertisement.js
@@ -3,13 +3,13 @@ const ads = [
     {
         title: "🎉 Congratulations to Our New Leaders!",
         text: "We are delighted to announce the newly selected Moderator of NDUB Computer Club. Let's welcome them with pride!",
-        img: "mod.png",
+        img: "news/mod.png",
         link: "about.html"  // link to event page
     },
     {
         title: "🎉 Congratulations to Our New Leaders!",
-        text: "We are delighted to announce the newly selected Moderator of NDUB Computer Club. Let's welcome them with pride!",
-        img: "co-mod.png",
+        text: "We are delighted to announce the newly selected Co-Moderator of NDUB Computer Club. Let's welcome them with pride!",
+        img: "news/co-mod.png",
         link: "about.html"  // link to event page
     }, 
     {
