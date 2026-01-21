038db3e45a2069b25d6483c9f768d32b45041307
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2026-01-21 21:54:10 +0600

    Update advertisement.js

--- Computer-Club/highlights/advertisement.js
+++ Computer-Club/highlights/advertisement.js
@@ -3,7 +3,7 @@ const ads = [
     {
         title: "📢 Club Registration Open",
         text: "Become a part of NDUB Computer Club and explore new opportunities!",
-        img: "assets/img/join-now-poster.png",
+        img: "assets/img/NDUBCC-Logo.png",
         link: "joinnow.html"
     }
     /*{
