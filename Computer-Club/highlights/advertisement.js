5d3c341fce791b3c741be9c5f62503777f49d335
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2026-04-08 19:50:42 +0600

    ads

--- Computer-Club/highlights/advertisement.js
+++ Computer-Club/highlights/advertisement.js
@@ -1,12 +1,12 @@
 // List of ads/news with links
 const ads = [
-    {
+    /*{
         title: "📢 Seminar On Understanding Complex Engineering Problems",
         text: "Bridging Academic Knowledge with Professional Engineering Competency!",
         img: "programs/seminar/cep/Poster.png",
         link: "programs/seminar/Complex-Engineering-Problems.html"
     }
-  /*{
+  {
         title: "📢 Club Registration Open",
         text: "Become a part of NDUB Computer Club and explore new opportunities!",
         img: "assets/img/NDUBCC-Logo.png",
